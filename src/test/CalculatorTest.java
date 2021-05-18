aec0f7926dfba01c0d2af11331b4f226a3e9961c
mbz1113zzz <1442638766@qq.com>
2021-05-18 22:41:21 +0800

    init

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void multipe() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Test
+    public void getResult() {
+    }
+}
